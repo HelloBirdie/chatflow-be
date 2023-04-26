4f7f371100a1bddd5753187434b95aea649a58b8
Jack Deng <Jack.z.deng@outlook.com>
2023-04-26 11:53:27 +0930

    update: git ignore (#1)

---
+++ src/main/java/com/hellobirdie/chatflow/ChatflowApplication.java
@@ -0,0 +1,13 @@
+package com.hellobirdie.chatflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hellobirdie/chatflow/ChatflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellobirdie.chatflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
