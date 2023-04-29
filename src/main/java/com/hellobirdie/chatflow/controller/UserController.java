88baa5ad4c010e79ffae17d3c380c42a3441e7b2
Skywuuuu <13003913026@163.com>
2023-04-29 21:11:57 +0800

    add sorting by userId

--- src/main/java/com/hellobirdie/chatflow/controller/UserController.java
+++ src/main/java/com/hellobirdie/chatflow/controller/UserController.java
@@ -4,14 +4,13 @@ import com.hellobirdie.chatflow.dto.user.UserGetDto;
 import com.hellobirdie.chatflow.dto.user.UserPostDto;
 import com.hellobirdie.chatflow.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
@@ -27,4 +26,9 @@ public class UserController {
 
     //TODO: Get all users profile. How to use spring boot to get user ID?
 
+    @GetMapping("/sortById")
+    public ResponseEntity<List<UserGetDto>> getSortedUserListById(@RequestParam(value = "isAscending", defaultValue = "true") Boolean isAscending) {
+        System.out.println(isAscending);
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getSortedUserListByID(isAscending));
+    }
 }
