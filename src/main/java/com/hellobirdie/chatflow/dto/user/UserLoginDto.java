b35488e8e554ec25704b3ebbcc8d26d899095a13
Skywuuuu <13003913026@163.com>
2023-05-07 10:18:58 +0800

    Update user login

--- src/main/java/com/hellobirdie/chatflow/dto/user/UserLoginDto.java
+++ src/main/java/com/hellobirdie/chatflow/dto/user/UserLoginDto.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UserLoginDto {
+    @NotBlank(message = "Email must not be blank.")
+    @Size(max = 255, message = "Email name can not be more than 255 characters.")
+    @Email(message = "Email should be a valid email.")
+    private String email;
+
     @NotEmpty(message = "Password must not be empty")
     @Pattern(regexp = "^(?=\\S*[a-zA-Z])(?=\\S*[0-9#!\"$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]).{8,}$",
             message = "Your password must be at least 8 character long and contains at least one non-letter character.")
