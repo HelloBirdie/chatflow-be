0292b95a18f0a3d29bf483991c290341366c734c
Skywuuuu <13003913026@163.com>
2023-05-07 10:08:41 +0800

    finish user login

--- src/main/java/com/hellobirdie/chatflow/dto/user/UserLoginDto.java
+++ src/main/java/com/hellobirdie/chatflow/dto/user/UserLoginDto.java
@@ -2,6 +2,7 @@ package com.hellobirdie.chatflow.dto.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
@@ -10,6 +11,7 @@ import javax.validation.constraints.Pattern;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginDto {
     @NotEmpty(message = "Password must not be empty")
     @Pattern(regexp = "^(?=\\S*[a-zA-Z])(?=\\S*[0-9#!\"$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]).{8,}$",
