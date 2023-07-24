9ddadd55a17b26a7124553790ead264f9cdbb7ca
Jack Deng <jack.z.deng@outlook.com>
2023-07-24 15:33:08 +0930

    add: google oauth login implementation

--- src/main/java/com/hellobirdie/chatflow/dto/user/UserPostDto.java
+++ src/main/java/com/hellobirdie/chatflow/dto/user/UserPostDto.java
@@ -1,6 +1,7 @@
 package com.hellobirdie.chatflow.dto.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import javax.validation.constraints.Size;
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class UserPostDto {
 
     @NotBlank
